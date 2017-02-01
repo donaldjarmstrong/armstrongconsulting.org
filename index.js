47f24fcc4c6a895d8c43f6f446cc0be401c68b1d
Donald Armstrong <donaldarmstrong@donniescbookpro.armstrong.net>
2017-01-31 20:33:43 -0600

    switched to node.js; added logging; added 404

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const path = require("path")
+const fs = require('fs')
+const pkg = require('./package')
+const Koa = require('koa')
+const Router = require('koa-router')
+const logger = require('koa-pino-logger')()
+
+const app = new Koa()
+const router = new Router()
+
+app.use(logger)
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+const readFile = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, fileName), { encoding: 'utf8' }, (err, data) => {
+      if (err) reject(err)
+      resolve(data)
+    })
+  })
+}
+
+router.get(['/resume.html', '/resume', 'resume.htm'], async function resume(ctx, next) {
+  ctx.body = await readFile('/resume.html')
+})
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+    const status = ctx.status || 404
+    if (status === 404) {
+        ctx.throw(404)
+    }
+  } catch (err) {
+    if (err.status === 404) {
+      // 404 handler
+      ctx.status = 404;
+      ctx.body = await readFile('/404.html')
+      return
+    }
+
+    ctx.status = err.status
+    ctx.body = err.message
+    app.emit('error', err, ctx)
+  }
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`${pkg.name} v${pkg.version} listening on port ${port}`);
+})
